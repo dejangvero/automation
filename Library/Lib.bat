f235417c4d6605b5941e8844aee0f9683dc833af
Dejan Gvero <dejangverobusiness@gmail.com>
2019-10-04 16:14:24 +0200

    Apaptor1

--- Library/Lib.bat
+++ Library/Lib.bat
@@ -1,7 +1,7 @@
 python -m pip install --upgrade pip
 pip install selenium
 pip install pytest
-pip install pytest-allure-adaptor2
+pip install pytest-allure-adaptor
 pip install unittest2
 pip install pytest-html
 pip install pytest-ordering
