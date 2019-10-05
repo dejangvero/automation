185497b7362561939da3b404e26da69c26a91740
Dejan Gvero <dejangverobusiness@gmail.com>
2019-10-05 19:19:24 +0200

    Test

--- Library/Lib.bat
+++ Library/Lib.bat
@@ -1,7 +1,6 @@
-python -m pip install --upgrade pip
 pip install selenium
 pip install pytest
-pip install --upgrade --force-reinstall allure-pytest
+pip install
 pip install unittest2
 pip install pytest-html
 pip install pytest-ordering
