a3178802917bbe0edb02a7a4440e2895b1f06536
Dejan Gvero <dejangverobusiness@gmail.com>
2019-10-05 21:23:53 +0200

    Final commit

--- Library/Lib.bat
+++ Library/Lib.bat
@@ -1,6 +1,6 @@
 pip list
 pip uninstall -y pytest-allure-adaptor
-pip uninstall -y pytest-allure-adaptor
+pip uninstall -y pytest-allure-adaptor2
 pip install selenium
 pip install pytest
 pip install allure-pytest
