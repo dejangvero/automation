56911889aebafffafa12744ce9a9cb2e2ee149aa
Dejan Gvero <dejangverobusiness@gmail.com>
2019-10-05 18:36:42 +0200

    HM

--- Library/Lib.bat
+++ Library/Lib.bat
@@ -1,7 +1,7 @@
 python -m pip install --upgrade pip
 pip install selenium
-pip install --upgrade --force-reinstall pytest
-pip install --upgrade --force-reinstall pytest-allure-adaptor
+pip install pytest
+pip install --upgrade --force-reinstall allure-pytest
 pip install unittest2
 pip install pytest-html
 pip install pytest-ordering
