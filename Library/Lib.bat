02e580bda7078a6a8865b8dbe0d6f21b230f6129
Dejan Gvero <dejangverobusiness@gmail.com>
2019-10-05 21:02:01 +0200

    Cleanup

--- Library/Lib.bat
+++ Library/Lib.bat
@@ -1,7 +1,19 @@
-pip install selenium
-pip install pytest
+pip uninstall selenium
+y
+pip uninstall pytest
+y
+pip uninstall pytest-allure-adaptor
+y
+pip uninstall pytest-allure-adaptor2
+y
+pip uninstall allure-pytest
+y
+pip uninstall allure-python-commons
+y
+pip install --upgrade --force-reinstall selenium
+pip install  --upgrade --force-reinstall pytest
 pip install --upgrade --force-reinstall allure-pytest
-pip install unittest2
-pip install pytest-html
-pip install pytest-ordering
-pip install ddt
+pip install --upgrade --force-reinstall unittest2
+pip install --upgrade --force-reinstall pytest-html
+pip install --upgrade --force-reinstall pytest-ordering
+pip install --upgrade --force-reinstall ddt
